8ebfa312cc88abef6454320b36283534fe1298bb
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-06-03 23:53:06 +0530

    simple card

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ function Note() {
   return (
     <div className="note">
       <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <p>Hello my Name is Basil</p>
     </div>
   );
 }
